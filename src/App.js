1afc13d57acd9206a343334ac801d8e01d51d8bd
enchanterv <vrnkmr55@gmail.com>
2021-03-23 20:30:09 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,50 +1,71 @@
-import React, { Component } from "react"
-import logo from "./logo.svg"
-import "./App.css"
+import { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
+import Coin from "./Coin";
 
-class LambdaDemo extends Component {
-  constructor(props) {
-    super(props)
-    this.state = { loading: false, msg: null }
-  }
+function App() {
+  const [coins, setCoins] = useState([]);
+  const [dataVisible, setDataVisible] = useState(false);
+  const [search, setSearch] = useState("");
 
-  handleClick = api => e => {
-    e.preventDefault()
+  useEffect(() => {
+    axios
+      .get(
+        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=inr&order=market_cap_desc&per_page=10&page=1&sparkline=false"
+      )
+      .then((res) => {
+        setCoins(res.data);
+        console.log(res.data);
+      })
+      .catch((error) => console.log(error));
+  }, []);
 
-    this.setState({ loading: true })
-    fetch("/.netlify/functions/" + api)
-      .then(response => response.json())
-      .then(json => this.setState({ loading: false, msg: json.msg }))
-  }
+  const handleChange = (e) => {
+    setSearch(e.target.value);
+  };
 
-  render() {
-    const { loading, msg } = this.state
-
-    return (
-      <p>
-        <button onClick={this.handleClick("hello")}>{loading ? "Loading..." : "Call Lambda"}</button>
-        <button onClick={this.handleClick("async-dadjoke")}>{loading ? "Loading..." : "Call Async Lambda"}</button>
-        <br />
-        <span>{msg}</span>
-      </p>
-    )
-  }
-}
-
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <LambdaDemo />
-        </header>
+  const filteredCoins = coins.filter((coin) =>
+    coin.name.toLowerCase().includes(search.toLowerCase())
+  );
+ let buttonTxt=  dataVisible ?"↑↑ Less Data":  "↓↓ More Data";
+  return (
+    <div className="coin-app">
+      <div className="coin-search">
+        <h1 className="coin-text">Currency Tracker</h1>
+        <form>
+          <input
+            className="coin-input"
+            type="text"
+            onChange={handleChange}
+            placeholder="Search"
+          />
+        </form>
+      </div>{" "}
+      <button
+        className="button-showData"
+        onClick={() => setDataVisible((pre) => !pre)}
+      >
+     {buttonTxt}
+      </button>{" "}
+      <div className="coin-upper-container">
+        {filteredCoins.map((coin) => {
+          return (
+            <Coin
+              key={coin.id}
+              name={coin.name}
+              price={coin.current_price}
+              symbol={coin.symbol}
+              marketcap={coin.total_volume}
+              volume={coin.market_cap}
+              image={coin.image}
+              priceChange={coin.price_change_percentage_24h}
+              dataVisible={dataVisible}
+            />
+          );
+        })}
       </div>
-    )
-  }
+    </div>
+  );
 }
 
-export default App
+export default App;
